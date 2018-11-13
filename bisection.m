1bf7f5a2038ee4e96435aa7195c85ecfbc8739b2
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-11-12 21:44:16 -0600

    Update bisection.m

--- bisection.m
+++ bisection.m
@@ -1,45 +1,37 @@
-%Input  : handle, min, max, #iteretions
-%Output sym: root, ea, vector, message
+%Parameters: handle, min, max, #iteretions
+%output    : root, ea, vector, message
 function [varargout] = bisection(h, min, max, it)
-
 handle = matlabFunction(evalin(symengine, h));
 Xi = min;
 Xu = max;
 is_continue = true;
 is_root = false;
-
 %verify if limits are roots
 if(handle(Xi)*handle(Xu) == 0)
-  is_continue = false;
-  
-  if(handle(Xi) == 0) 
-    varargout{1} = Xi;
-  else
-    varargout{1} = Xu;
-  end
-
-  varargout{2} = 0;
-  varargout{3} = [0];
-  varargout{4} = 'Success!';
-
-%verify if limits are correct (positive and negative)
+    is_continue = false;
+    if(handle(Xi) == 0)
+        varargout{1} = Xi;
+    else
+        varargout{1} = Xu;
+    end
+    varargout{2} = 0;
+    varargout{3} = 0;
+    varargout{4} = 'Success!';
 else
-  if(handle(Xi)*handle(Xu) > 0)
-      is_continue = false;
-
-      varargout{1} = 0;
-      varargout{2} = 0;
-      varargout{3} = [0];
-      varargout{4} = 'Both evaluated limits have the same sign.';
-  end
+    %verify if limits are correct (positive and negative)
+    if(handle(Xi)*handle(Xu) > 0)
+        is_continue = false;
+        varargout{1} = 0;
+        varargout{2} = 0;
+        varargout{3} = 0;
+        varargout{4} = 'Try with other limits.';
+    end
 end
-
-while(is_continue)
-    iteration = it;
+if(is_continue)
     counter = 1;
     ea = 0;
     varargout{3} = [];
-    while(counter <= iteration)
+    while(counter <= it)
         Xr = (Xi + Xu) / 2;
         if(counter > 1)
             ea = abs((Xr - ea) / Xr) * 100;
@@ -47,29 +39,25 @@ while(is_continue)
         end
         if(round(handle(Xr),4)== 0)
             varargout{1} = Xr;
-            counter = iteration;
+            counter = it;
             is_root = true;
             varargout{4} = 'Success!';
-            is_continue = false;
         elseif(handle(Xr)*handle(Xi) < 0)
             Xu = Xr;
         elseif(handle(Xr)*handle(Xi) > 0)
             Xi = Xr;
         end
-        ea = Xr;
         varargout{2} = ea;
+        ea = Xr;
         counter = counter + 1;
     end
     if(~is_root)
         varargout{1} = Xr;
         if(Xi == Xu)
             varargout{4} = 'Try with other limits.';
-            is_continue = false;
         else
             varargout{4} = 'Try with more iterations.';
-            is_continue = false;
         end
     end
 end
-
-display(varargout{3});
+end
